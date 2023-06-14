ed5a80c8e3c5d86f70f6136e5ff712abc7c0c63f
bmaggiano <brandon.maggiano@gmail.com>
2023-06-14 13:03:01 -0700

    added test page in src/app/test/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>Super</h1>
+      <h1>Super <span>Showdown</span></h1>
     </main>
   )
 }
---
+++ src/app/test/page.tsx
@@ -0,0 +1,12 @@
+// for a new page, we don't need react router dom
+// just create a new folder inside the src/app directory and create a page.tsx inside that folder
+// So this page will be available at localhost:3000/test
+import React from 'react'
+
+export default function Page() {
+  return (
+    <div>
+      <h1>test</h1>
+    </div>
+  )
+}
