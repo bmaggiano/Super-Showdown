963fd8c62a4233685230791678aee8c7c9e721dc
bmaggiano <brandon.maggiano@gmail.com>
2023-06-27 11:22:08 -0700

    made landing page the home page after user logs in, and the former main page moved to playNow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,321 +1,127 @@
-"use client";
+"use client"
+import { useState } from 'react'
+import { Dialog } from '@headlessui/react'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+import { UserButton } from '@clerk/clerk-react'
 
-import React, { useState } from "react";
-import Head from "next/head";
-import {
-  Heart,
-  Brain,
-  Barbell,
-  BatteryFull,
-  HandFist,
-  Wind,
-} from "phosphor-react";
-import styles from "./page.module.css";
-import User from "./userButton/page";
-import { UserButton } from "@clerk/clerk-react";
+const navigation = [
+  { name: 'Leaderboard', href: '/getPrisma' },
+]
 
-export default function PlayGame() {
-  const [responseText, setResponseText] = useState("");
-  const [firstHero, setFirstHero] = useState<any>([]);
-  const [userChoice, setUserChoice] = useState<any>("");
-  const [secondHero, setSecondHero] = useState<any>(null);
-
-  // define the type of data that gets brought in to prevent future errors
-  interface Hero {
-    id: number;
-    name: string;
-    image: {
-      url: string;
-    };
-    powerstats: {
-      intelligence: string;
-      strength: string;
-      speed: string;
-      durability: string;
-      power: string;
-      combat: string;
-    };
-    biography: {
-      "full-name": string;
-    };
-  }
-
-  const handleFirstHero = async (e: any) => {
-    e.preventDefault();
-
-    const response = await fetch("/api/threeCharacters", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-
-    if (response.ok) {
-      const data = await response.json();
-      setFirstHero(data);
-    } else {
-      console.log("Error occurred:", response.status);
-    }
-  };
-
-  const handleSecondHero = async (e: any) => {
-    e.preventDefault();
-
-    const response = await fetch("/api/oponent", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-
-    if (response.ok) {
-      const data = await response.json();
-      setSecondHero(data);
-    } else {
-      console.log("Error occurred:", response.status);
-    }
-    console.log(secondHero);
-  };
-
-  const handleClick = async (e: any) => {
-    e.preventDefault();
-
-    const prompt = `Without any explanation, who would win between ${userChoice} vs. ${secondHero?.name}? Then give a one sentence explanation of how this character would win.`;
-
-    const response = await fetch("/api/generate", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        prompt,
-      }),
-    });
-
-    if (!response.ok) {
-      throw new Error(response.statusText);
-    }
-
-    const data = await response.json(); // Parse the response as JSON
-
-    const responseText = data.response; // Access the response value from the JSON object
-
-    setResponseText(responseText);
-  };
+export default function Example() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
-    <>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="true"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Comic+Neue:ital,wght@0,400;1,400;1,700&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-
-      <div className="m-3">
-      <UserButton />
-      </div>
-      {/* <CurrentUserInfo/> */}
-
-      <main className={styles.main}>
-        <h1>
-          Super <span>Showdown</span>
-        </h1>
-        <br />
-      </main>
-
-      <div className={styles.firstAction}>
-        {firstHero.length === 0 && (
-          <button onClick={(e) => handleFirstHero(e)}>Superhero 1</button>
-        )}
-        {firstHero.length > 0 && (
-          <>
-            <h2>Choose your character!</h2>
-            <span>you only get one shot at this!</span>
-          </>
-        )}
-      </div>
-      <section className={styles.randomSelector}>
-        {firstHero &&
-          firstHero.map((hero: Hero) => (
-            <div key={hero?.id} className={styles.characterCard}>
-              <div className={styles.cardHeader}>
-                <h3>{hero?.name}</h3>
-                <span>{hero?.biography["full-name"]}</span>
+    <div className="bg-white">
+      <header className="absolute inset-x-0 top-0 z-50">
+        <div className="mx-auto max-w-7xl">
+          <div className="px-6 pt-6 lg:max-w-2xl lg:pl-8 lg:pr-0">
+            <nav className="flex items-center justify-between lg:justify-start" aria-label="Global">
+              <UserButton/>
+              <button
+                type="button"
+                className="-m-2.5 rounded-md p-2.5 text-gray-700 lg:hidden"
+                onClick={() => setMobileMenuOpen(true)}
+              >
+                <span className="sr-only">Open main menu</span>
+                <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+              </button>
+              <div className="hidden lg:ml-12 lg:flex lg:gap-x-14">
+                {navigation.map((item) => (
+                  <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-gray-900">
+                    {item.name}
+                  </a>
+                ))}
               </div>
-              <br />
-              <img
-                className={styles.heroPic}
-                src={hero?.image.url}
-                alt={hero?.name}
-              />
-              {hero.powerstats.intelligence !== "null" ? (
-                <>
-                  <div className={styles.characterData}>
-                    <div className={styles.powerStats}>
-                      <div className={styles.powerStatsGroup}>
-                        <p>
-                          <span className={styles.icon}>
-                            <Brain color="pink" weight="duotone" size={20} />
-                          </span>
-                          : {hero?.powerstats?.intelligence}
-                        </p>
-                        <p>
-                          <span className={styles.icon}>
-                            <Barbell color="white" weight="duotone" size={20} />
-                          </span>
-                          : {hero?.powerstats?.strength}
-                        </p>
-                        <p>
-                          <span className={styles.icon}>
-                            <Wind color="yellow" weight="fill" size={20} />
-                          </span>
-                          : {hero?.powerstats?.speed}
-                        </p>
-                      </div>
-                      <div className={styles.powerStatsGroup}>
-                        <p>
-                          <span className={styles.icon}>
-                            <BatteryFull
-                              color="green"
-                              weight="duotone"
-                              size={20}
-                            />
-                          </span>
-                          : {hero?.powerstats.power}
-                        </p>
-                        <p>
-                          <span className={styles.icon}>
-                            <Heart color="#AE2983" weight="fill" size={20} />
-                          </span>
-                          : {hero?.powerstats?.durability}
-                        </p>
-                        <p>
-                          <span className={styles.icon}>
-                            <HandFist color="white" weight="fill" size={20} />
-                          </span>
-                          : {hero?.powerstats?.combat}
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                </>
-              ) : (
-                <>
-                  <p>Powerstats not available</p>
-                </>
-              )}
-              {!userChoice && (
-                <button
-                  className="userPick"
-                  onClick={() => setUserChoice(hero?.name)}
-                >
-                  Choose: {hero?.name}
-                </button>
-              )}
-            </div>
-          ))}
-      </section>
-
-      <div>
-        <div className={styles.firstAction}>
-          {!secondHero && (
-            <button onClick={(e) => handleSecondHero(e)}>Superhero 2</button>
-          )}
-          {userChoice && secondHero && (
-            <>
-              <div className={styles.whoWins}>
-                <button onClick={(e) => handleClick(e)}>
-                  Who would win between <span>{userChoice}</span> vs.{" "}
-                  <span>{secondHero?.name}</span>?
-                </button>
-              </div>
-              {responseText && (
-                <div className={styles.responseContainer}>
-                  <h4>{responseText}</h4>
-                </div>
-              )}
-            </>
-          )}
-        </div>
-        {secondHero && (
-          <div className={styles.characterCard}>
-            <div className={styles.cardHeader}>
-              <h3>{secondHero?.name}</h3>
-              <span>{secondHero?.biography["full-name"]}</span>
-            </div>
-            <br />
-            <img
-              className={styles.heroPic}
-              src={secondHero?.image.url}
-              alt={secondHero?.name}
-            />
-            {secondHero.powerstats.intelligence !== "null" ? (
-              <>
-                <div className={styles.characterData}>
-                  <div className={styles.powerStats}>
-                    <div className={styles.powerStatsGroup}>
-                      <p>
-                        <span className={styles.icon}>
-                          <Brain color="pink" weight="duotone" size={20} />
-                        </span>
-                        : {secondHero?.powerstats?.intelligence}
-                      </p>
-                      <p>
-                        {" "}
-                        <span className={styles.icon}>
-                          <Barbell color="white" weight="duotone" size={20} />
-                        </span>
-                        : {secondHero?.powerstats?.strength}
-                      </p>
-                      <p>
-                        <span className={styles.icon}>
-                          <Wind color="yellow" weight="fill" size={20} />
-                        </span>
-                        : {secondHero?.powerstats?.speed}
-                      </p>
-                    </div>
-                    <div className={styles.powerStatsGroup}>
-                      <p>
-                        <span className={styles.icon}>
-                          <BatteryFull
-                            color="green"
-                            weight="duotone"
-                            size={20}
-                          />
-                        </span>
-                        : {secondHero?.powerstats.power}
-                      </p>
-                      <p>
-                        <span className={styles.icon}>
-                          <Heart color="#AE2983" weight="fill" size={20} />
-                        </span>
-                        : {secondHero?.powerstats?.durability}
-                      </p>
-                      <p>
-                        <span className={styles.icon}>
-                          <HandFist color="white" weight="fill" size={20} />
-                        </span>
-                        : {secondHero?.powerstats?.combat}
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </>
-            ) : (
-              <>
-                <p>Powerstats not available</p>
-              </>
-            )}
+            </nav>
           </div>
-        )}
+        </div>
+        <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+          <div className="fixed inset-0 z-50" />
+          <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+            <div className="flex items-center justify-between">
+            <UserButton/>
+              <button
+                type="button"
+                className="-m-2.5 rounded-md p-2.5 text-gray-700"
+                onClick={() => setMobileMenuOpen(false)}
+              >
+                <span className="sr-only">Close menu</span>
+                <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+              </button>
+            </div>
+            <div className="mt-6 flow-root">
+              <div className="-my-6 divide-y divide-gray-500/10">
+                <div className="space-y-2 py-6">
+                  {navigation.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                    >
+                      {item.name}
+                    </a>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </Dialog.Panel>
+        </Dialog>
+      </header>
+
+      <div className="relative">
+        <div className="mx-auto max-w-7xl">
+          <div className="relative z-10 pt-14 lg:w-full lg:max-w-2xl">
+            <svg
+              className="absolute inset-y-0 right-8 hidden h-full w-80 translate-x-1/2 transform fill-white lg:block"
+              viewBox="0 0 100 100"
+              preserveAspectRatio="none"
+              aria-hidden="true"
+            >
+              <polygon points="0,0 90,0 50,100 0,100" />
+            </svg>
+
+            <div className="relative px-6 py-32 sm:py-40 lg:px-8 lg:py-56 lg:pr-0">
+              <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-xl">
+                <div className="hidden sm:mb-10 sm:flex">
+                  <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-500 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+                    Click here to check out some of my other projects{' '}
+                    <a href="https://github.com/bmaggiano" target="_blank" className="whitespace-nowrap font-semibold text-slate-600">
+                      <span className="absolute inset-0" aria-hidden="true" />
+                      GitHub <span aria-hidden="true">&rarr;</span>
+                    </a>
+                  </div>
+                </div>
+                <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+                  Super Showdown
+                </h1>
+                <p className="mt-6 text-lg leading-8 text-gray-600">
+                  A card battle game that relies on AI, instinct, and individual intuition to beat your opponent
+                  and claim the number one spot on the leaderboard!
+                </p>
+                <div className="mt-10 flex items-center gap-x-6">
+                  <a
+                    href="/playNow"
+                    className="rounded-md bg-slate-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-slate-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  >
+                    Play Now
+                  </a>
+                  <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+                    Learn more <span aria-hidden="true">→</span>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="bg-gray-50 lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+          <img
+            className="aspect-[3/2] object-cover lg:aspect-auto lg:h-full lg:w-full"
+            src="./vs.jpg"
+            alt=""
+          />
+        </div>
       </div>
-    </>
-  );
+    </div>
+  )
 }
---
+++ src/app/playNow/page.tsx
@@ -0,0 +1,320 @@
+"use client";
+
+import React, { useState } from "react";
+import Head from "next/head";
+import {
+  Heart,
+  Brain,
+  Barbell,
+  BatteryFull,
+  HandFist,
+  Wind,
+} from "phosphor-react";
+import styles from "../page.module.css";
+import { UserButton } from "@clerk/clerk-react";
+
+export default function PlayGame() {
+  const [responseText, setResponseText] = useState("");
+  const [firstHero, setFirstHero] = useState<any>([]);
+  const [userChoice, setUserChoice] = useState<any>("");
+  const [secondHero, setSecondHero] = useState<any>(null);
+
+  // define the type of data that gets brought in to prevent future errors
+  interface Hero {
+    id: number;
+    name: string;
+    image: {
+      url: string;
+    };
+    powerstats: {
+      intelligence: string;
+      strength: string;
+      speed: string;
+      durability: string;
+      power: string;
+      combat: string;
+    };
+    biography: {
+      "full-name": string;
+    };
+  }
+
+  const handleFirstHero = async (e: any) => {
+    e.preventDefault();
+
+    const response = await fetch("/api/threeCharacters", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      setFirstHero(data);
+    } else {
+      console.log("Error occurred:", response.status);
+    }
+  };
+
+  const handleSecondHero = async (e: any) => {
+    e.preventDefault();
+
+    const response = await fetch("/api/oponent", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      setSecondHero(data);
+    } else {
+      console.log("Error occurred:", response.status);
+    }
+    console.log(secondHero);
+  };
+
+  const handleClick = async (e: any) => {
+    e.preventDefault();
+
+    const prompt = `Without any explanation, who would win between ${userChoice} vs. ${secondHero?.name}? Then give a one sentence explanation of how this character would win.`;
+
+    const response = await fetch("/api/generate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt,
+      }),
+    });
+
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+
+    const data = await response.json(); // Parse the response as JSON
+
+    const responseText = data.response; // Access the response value from the JSON object
+
+    setResponseText(responseText);
+  };
+
+  return (
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Comic+Neue:ital,wght@0,400;1,400;1,700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+
+      <div className="m-3">
+      <UserButton />
+      </div>
+      {/* <CurrentUserInfo/> */}
+
+      <main className={styles.main}>
+        <h1>
+          Super <span>Showdown</span>
+        </h1>
+        <br />
+      </main>
+
+      <div className={styles.firstAction}>
+        {firstHero.length === 0 && (
+          <button onClick={(e) => handleFirstHero(e)}>Superhero 1</button>
+        )}
+        {firstHero.length > 0 && (
+          <>
+            <h2>Choose your character!</h2>
+            <span>you only get one shot at this!</span>
+          </>
+        )}
+      </div>
+      <section className={styles.randomSelector}>
+        {firstHero &&
+          firstHero.map((hero: Hero) => (
+            <div key={hero?.id} className={styles.characterCard}>
+              <div className={styles.cardHeader}>
+                <h3>{hero?.name}</h3>
+                <span>{hero?.biography["full-name"]}</span>
+              </div>
+              <br />
+              <img
+                className={styles.heroPic}
+                src={hero?.image.url}
+                alt={hero?.name}
+              />
+              {hero.powerstats.intelligence !== "null" ? (
+                <>
+                  <div className={styles.characterData}>
+                    <div className={styles.powerStats}>
+                      <div className={styles.powerStatsGroup}>
+                        <p>
+                          <span className={styles.icon}>
+                            <Brain color="pink" weight="duotone" size={20} />
+                          </span>
+                          : {hero?.powerstats?.intelligence}
+                        </p>
+                        <p>
+                          <span className={styles.icon}>
+                            <Barbell color="white" weight="duotone" size={20} />
+                          </span>
+                          : {hero?.powerstats?.strength}
+                        </p>
+                        <p>
+                          <span className={styles.icon}>
+                            <Wind color="yellow" weight="fill" size={20} />
+                          </span>
+                          : {hero?.powerstats?.speed}
+                        </p>
+                      </div>
+                      <div className={styles.powerStatsGroup}>
+                        <p>
+                          <span className={styles.icon}>
+                            <BatteryFull
+                              color="green"
+                              weight="duotone"
+                              size={20}
+                            />
+                          </span>
+                          : {hero?.powerstats.power}
+                        </p>
+                        <p>
+                          <span className={styles.icon}>
+                            <Heart color="#AE2983" weight="fill" size={20} />
+                          </span>
+                          : {hero?.powerstats?.durability}
+                        </p>
+                        <p>
+                          <span className={styles.icon}>
+                            <HandFist color="white" weight="fill" size={20} />
+                          </span>
+                          : {hero?.powerstats?.combat}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </>
+              ) : (
+                <>
+                  <p>Powerstats not available</p>
+                </>
+              )}
+              {!userChoice && (
+                <button
+                  className="userPick"
+                  onClick={() => setUserChoice(hero?.name)}
+                >
+                  Choose: {hero?.name}
+                </button>
+              )}
+            </div>
+          ))}
+      </section>
+
+      <div>
+        <div className={styles.firstAction}>
+          {!secondHero && (
+            <button onClick={(e) => handleSecondHero(e)}>Superhero 2</button>
+          )}
+          {userChoice && secondHero && (
+            <>
+              <div className={styles.whoWins}>
+                <button onClick={(e) => handleClick(e)}>
+                  Who would win between <span>{userChoice}</span> vs.{" "}
+                  <span>{secondHero?.name}</span>?
+                </button>
+              </div>
+              {responseText && (
+                <div className={styles.responseContainer}>
+                  <h4>{responseText}</h4>
+                </div>
+              )}
+            </>
+          )}
+        </div>
+        {secondHero && (
+          <div className={styles.characterCard}>
+            <div className={styles.cardHeader}>
+              <h3>{secondHero?.name}</h3>
+              <span>{secondHero?.biography["full-name"]}</span>
+            </div>
+            <br />
+            <img
+              className={styles.heroPic}
+              src={secondHero?.image.url}
+              alt={secondHero?.name}
+            />
+            {secondHero.powerstats.intelligence !== "null" ? (
+              <>
+                <div className={styles.characterData}>
+                  <div className={styles.powerStats}>
+                    <div className={styles.powerStatsGroup}>
+                      <p>
+                        <span className={styles.icon}>
+                          <Brain color="pink" weight="duotone" size={20} />
+                        </span>
+                        : {secondHero?.powerstats?.intelligence}
+                      </p>
+                      <p>
+                        {" "}
+                        <span className={styles.icon}>
+                          <Barbell color="white" weight="duotone" size={20} />
+                        </span>
+                        : {secondHero?.powerstats?.strength}
+                      </p>
+                      <p>
+                        <span className={styles.icon}>
+                          <Wind color="yellow" weight="fill" size={20} />
+                        </span>
+                        : {secondHero?.powerstats?.speed}
+                      </p>
+                    </div>
+                    <div className={styles.powerStatsGroup}>
+                      <p>
+                        <span className={styles.icon}>
+                          <BatteryFull
+                            color="green"
+                            weight="duotone"
+                            size={20}
+                          />
+                        </span>
+                        : {secondHero?.powerstats.power}
+                      </p>
+                      <p>
+                        <span className={styles.icon}>
+                          <Heart color="#AE2983" weight="fill" size={20} />
+                        </span>
+                        : {secondHero?.powerstats?.durability}
+                      </p>
+                      <p>
+                        <span className={styles.icon}>
+                          <HandFist color="white" weight="fill" size={20} />
+                        </span>
+                        : {secondHero?.powerstats?.combat}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </>
+            ) : (
+              <>
+                <p>Powerstats not available</p>
+              </>
+            )}
+          </div>
+        )}
+      </div>
+    </>
+  );
+}
