a0adcd0662aed21b20526d3a633241747e9a7276
bmaggiano <brandon.maggiano@gmail.com>
2023-06-28 09:27:18 -0700

    attempted to change play now head font

--- src/app/playHead/page.tsx
+++ src/app/playHead/page.tsx
@@ -24,7 +24,7 @@ const features = [
   
         <div className="relative mx-auto -mt-12 max-w-7xl px-4 pb-16 sm:px-6 sm:pb-24 lg:px-8">
           <div className="mx-auto max-w-2xl text-center lg:max-w-4xl">
-            <h2 className="text-3xl font-bold tracking-wide text-gray-900 sm:text-4xl">Let's get ready to rumble!</h2>
+            <h1 className="text-4xl font-bold tracking-wide text-gray-900 sm:text-5xl">Let's get ready to rumble!</h1>
             <p className="mt-4 text-gray-500">
               Organize is a system to keep your desk tidy and photo-worthy all day long. Procrastinate your work while you
               meticulously arrange items into dedicated trays.
