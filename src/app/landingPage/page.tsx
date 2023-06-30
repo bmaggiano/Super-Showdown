cc2e115c03ed6b875af2e756141b57acc680d753
bmaggiano <brandon.maggiano@gmail.com>
2023-06-29 17:03:40 -0700

    added scroll to section id of play now to learn more a tag

--- src/app/landingPage/page.tsx
+++ src/app/landingPage/page.tsx
@@ -90,7 +90,7 @@ export default function LandingPage() {
                     Play Now
                   </button>
                   <a
-                    href="#"
+                    href="#howToPlay"
                     className="text-sm font-semibold leading-6 text-gray-900"
                   >
                     Learn more <span aria-hidden="true">→</span>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,9 @@ export default function Home() {
   return (
     <>
       <LandingPage/>
+      <section id="howToPlay">
       <LandingPageHero/>
+      </section>
       <Footer/>
     </>
   )
