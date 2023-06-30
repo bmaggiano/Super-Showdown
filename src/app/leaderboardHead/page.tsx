3adaa287aa98caa20e1d5f72e1a0fcf1dc8be4cf
bmaggiano <brandon.maggiano@gmail.com>
2023-06-30 11:52:35 -0700

    updated leaderboard head for a new space like picture and blurred to allow user to read text

--- src/app/leaderboardHead/page.tsx
+++ src/app/leaderboardHead/page.tsx
@@ -1,17 +1,21 @@
+"use client"
+
+// Page header from tailwind UI that gives professional header look
+
 export default function LeaderboardHead() {
     return (
       <div className="relative isolate overflow-hidden bg-gray-900 px-6 py-24 sm:py-32 lg:px-8">
         <img
-          src="./leaderboard.jpg"
+          src="./space.jpg"
           alt=""
-          className="absolute inset-0 -z-10 h-full w-full object-cover"
+          className="blur-sm absolute inset-0 -z-10 h-full w-full object-cover"
         />
         <div
           className="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl"
           aria-hidden="true"
         >
           <div
-            // className="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20"
+            className="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff0000] to-[#0d00ff] opacity-20"
             style={{
               clipPath:
                 'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
@@ -23,7 +27,7 @@ export default function LeaderboardHead() {
           aria-hidden="true"
         >
           <div
-            className="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20"
+            className="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff0000] to-[#0d00ff] opacity-20"
             style={{
               clipPath:
                 'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
